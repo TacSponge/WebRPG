565fa37650803cbdc80c7caa6ecf241db63acb88
Miller Adams <miller.adams@alcidion.com>
2021-08-11 11:12:48 +1200

    add onclick handler to attack button

--- UI/src/components/FightPanel.jsx
+++ UI/src/components/FightPanel.jsx
@@ -2,12 +2,22 @@ import React, { Component } from "react";
 
 class FightPanel extends Component {
   state = { creature: { name: "Goblin", HP: 10 } };
+
+  onAttack = () => {
+    const creature = { ...this.state.creature };
+    creature.HP = this.state.creature.HP - 1;
+    console.log(creature);
+    console.log(this.state);
+    this.setState({ creature });
+  };
+
   render() {
     return (
       <div>
         <h1>You are fighting a {this.state.creature.name}</h1>
+        <p> The creature has {this.state.creature.HP} HP</p>
         <p>Click the button to attack</p>
-        <button>Attack!</button>
+        <button onClick={this.onAttack}>Attack!</button>
       </div>
     );
   }
