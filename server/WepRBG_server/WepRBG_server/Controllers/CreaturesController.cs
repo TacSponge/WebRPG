5bab6a10eed1fb32c886100a5dcbbabd49c55b3e
Miller Adams <miller.adams@alcidion.com>
2021-08-12 13:07:08 +1200

    initial commit of server

---
+++ server/WepRBG_server/WepRBG_server/Controllers/CreaturesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WepRBG_server.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WepRBG_server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreaturesController : ControllerBase
+    {
+        // GET: <ValuesController>
+        [HttpGet]
+        public Creature Get()
+        {          
+            return new Creature("Moblin", 12);
+        }
+    }
+}
---
+++ server/WepRBG_server/WepRBG_server/Models/Creature.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WepRBG_server.Models
+{
+    public class Creature
+    {
+        public String Name { get; protected set; }
+        public int HP { get; protected set; }
+        
+        public Creature(String name, int HP)
+        {
+            Name = name;
+            this.HP = HP;
+        }
+    }
+}
