a7cca157651d960fa2e8223c7206869ed86a910e
Miller Adams <miller.adams@alcidion.com>
2021-08-16 10:37:50 +1200

    allow main screen to switch between admin and game panel

--- UI/src/components/MainScreen.jsx
+++ UI/src/components/MainScreen.jsx
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
 import GamePanel from "./GamePanel";
 import TabNavigator from "./TabNavigator";
+import AdminScreen from "./AdminScreen";
 
 class MainScreen extends Component {
   state = { tabIndex: 0 };
   tabs = [0, 1];
 
+  tabHandler = (i) => {
+    console.log("Selected tab " + i);
+    this.setState({ tabIndex: i });
+  };
+
   render() {
     return (
       <div>
-        <TabNavigator tabs={this.tabs} />
-        <GamePanel />
+        <TabNavigator tabs={this.tabs} onTabSelect={this.tabHandler} />
+        {this.state.tabIndex == 0 && <GamePanel />}
+        {this.state.tabIndex == 1 && <AdminScreen />}
       </div>
     );
   }
